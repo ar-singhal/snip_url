02d2ebf10d3a75ef35ca2c03d75def274d612ac5
ar-singhal <aryansinghal123457@gmail.com>
2022-08-15 19:22:37 +0530

    first commit

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,24 @@
+
+const BackgroundAnimate = () => {
+  return (
+    <div className="bubbles">
+    <ul >
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      <li className="bubble"></li>
+      
+    </ul>
+    </div>
+  )
+}
+
+export default BackgroundAnimate
